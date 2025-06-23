2f36d00f41b4cc2b423399a38f7f8e36959c2f0b
Lek Jing Kai <lekjingkai@hotmail.com>
2025-06-23 23:59:15 +0800

    clean up

--- src/api/client.ts
+++ src/api/client.ts
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL ?? 'http://localhost:4000',
     headers: {
         'Content-Type': 'application/json',
-        'X-MANDAI-USER-ID': '3f0a1c2d4b5e6f7a8b9c0d1e2f3a4b5c',
+        'X-MANDAI-USER-ID': process.env.REACT_APP_USER_ID ?? 'test-uuid',
     },
 });
 
